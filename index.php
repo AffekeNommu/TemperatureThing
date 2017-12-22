89b5cd28f32b8709c48d7288e16615c690108cd1
AffekeNommu <32345567+AffekeNommu@users.noreply.github.com>
2017-12-23 08:44:47 +1100

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "logging user name";
+$password = "logging user password";
+$dbname = "Temperature";
+$data=array();
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname, 0, '/path/to/your/database/mysql/socket');
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT TimeStamp, Temperature FROM Readings where date(TimeStamp)= date(addtime(now(),'11:0:0')) and Source='sensorID' order by TimeStamp desc";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+    echo '<html><head></head><body><a href="month.html">Month</a><table border=1><tr><th>Time</th><th>Temperature</th></tr>';
+    while($row = $result->fetch_assoc()) {
+    echo "<tr align=center><td>";
+    echo $row['TimeStamp'];
+    echo "</td><td>";
+    echo $row['Temperature'];
+    echo "</td></tr>";
+    }
+    echo "</table></body></html>";
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
