6b1ea4d812dd192cdad5d40b7e3881b3db46656c
AffekeNommu <32345567+AffekeNommu@users.noreply.github.com>
2017-12-23 08:46:26 +1100

    Create inserto.php

---
+++ inserto.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+  <head>
+    <title>Inserto</title>
+  </head>
+  <body>
+<?php
+$servername = "localhost";
+$username = "temperature log user";
+$password = "temperature log password";
+$dbname = "Temperature";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname, 0, '/path/to/your/database/mysql/socket');
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$Source=$_GET["Source"]; 
+$Temperature=$_GET["Temperature"];
+$TimeStamp=$_GET["TimeStamp"];
+$sql = "INSERT INTO Readings (Source, Temperature, TimeStamp)
+VALUES ('$Source','$Temperature', '$TimeStamp')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
+  </body>
+</html>
