a96ffd81caea100179cc9870f30d2fe649d5853c
AffekeNommu <32345567+AffekeNommu@users.noreply.github.com>
2018-01-21 13:25:57 +1100

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $sql = "SELECT TimeStamp, Temperature FROM Readings where date(TimeStamp)= date(
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
-    echo '<html><head></head><body><a href="month.html">Month</a><table border=1><tr><th>Time</th><th>Temperature</th></tr>';
+    echo '<html><head></head><body><a href="month.php">Month</a><table border=1><tr><th>Time</th><th>Temperature</th></tr>';
     while($row = $result->fetch_assoc()) {
     echo "<tr align=center><td>";
     echo $row['TimeStamp'];
